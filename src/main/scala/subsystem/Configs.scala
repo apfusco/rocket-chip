2f7619bd105725fd24f6dbe570ede93c1a877994
Alexander Fusco <afusco1@arizona.edu>
2023-03-22 14:53:59 -0700

    Configs: Add soft core
    Same as small core, but includes VM.

--- src/main/scala/subsystem/Configs.scala
+++ src/main/scala/subsystem/Configs.scala
@@ -134,6 +134,32 @@ class WithNMedCores(n: Int, overrideIdOffset: Option[Int] = None) extends Config
   }
 })
 
+class WithNSoftCores(n: Int, overrideIdOffset: Option[Int] = None) extends Config((site, here, up) => {
+  case RocketTilesKey => {
+    val prev = up(RocketTilesKey, site)
+    val idOffset = overrideIdOffset.getOrElse(prev.size)
+    val small = RocketTileParams(
+      core = RocketCoreParams(useVM = true, fpu = None),
+      btb = None,
+      dcache = Some(DCacheParams(
+        rowBits = site(SystemBusKey).beatBits,
+        nSets = 64,
+        nWays = 1,
+        nTLBSets = 1,
+        nTLBWays = 4,
+        nMSHRs = 0,
+        blockBytes = site(CacheBlockBytes))),
+      icache = Some(ICacheParams(
+        rowBits = site(SystemBusKey).beatBits,
+        nSets = 64,
+        nWays = 1,
+        nTLBSets = 1,
+        nTLBWays = 4,
+        blockBytes = site(CacheBlockBytes))))
+    List.tabulate(n)(i => small.copy(hartId = i + idOffset)) ++ prev
+  }
+})
+
 class WithNSmallCores(n: Int, overrideIdOffset: Option[Int] = None) extends Config((site, here, up) => {
   case RocketTilesKey => {
     val prev = up(RocketTilesKey, site)
